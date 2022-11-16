5ac13375d969af920d732037b2bbd30ba4558fec
unknown <archraphr@gmail.com>
2022-11-16 07:11:52 -0800

    serviceUpdate

--- src/Components/ContactUs.js
+++ src/Components/ContactUs.js
@@ -1,55 +1,63 @@
-import React from 'react'
-import CenteredHeadingUnderline from './CenteredHeadingUnderline'
-import Lists from '../Components/Lists'
+import React from "react";
+import CenteredHeadingUnderline from "./CenteredHeadingUnderline";
+import Lists from "../Components/Lists";
 
 var contactHandles = [
   <a
-    href='https://web.facebook.com/raphael.tunji2/'
-    target='_blank'
-    rel='noreferrer'
+    href="https://web.facebook.com/raphael.tunji2/"
+    target="_blank"
+    rel="noreferrer"
   >
-    <i class="fa fa-facebook-square"></i> Facebook
+    <i class="fa fa-facebook-square"></i>{" "}
+    <span class="hideContact">Facebook</span>
   </a>,
-  <a href='https://twitter.com/ArchIntel1' target='_blank' rel='noreferrer'>
-    <i class="fa fa-twitter"></i> Twitter
+  <a href="https://twitter.com/ArchIntel1" target="_blank" rel="noreferrer">
+    <i class="fa fa-twitter"></i> <span class="hideContact">Twitter</span>
   </a>,
-  <a href='https://www.instagram.com/' target='_blank' rel='noreferrer'>
-    <i class="fa fa-instagram"></i> Instagram
+  <a href="https://www.instagram.com/" target="_blank" rel="noreferrer">
+    <i class="fa fa-instagram"></i> <span class="hideContact">Instagram</span>
   </a>,
   <a
-    href='http://www.terawork.com/home/archraphael'
-    target='_blank'
-    rel='noreferrer'
+    href="http://www.terawork.com/home/archraphael"
+    target="_blank"
+    rel="noreferrer"
   >
-     <img src="./terawork.png" alt="error" style={{width: '30px'}}></img> TeraWork
+    <img src="./terawork.png" alt="error" style={{ width: "30px" }}></img>{" "}
+    <span class="hideContact">TeraWork</span>
   </a>,
   <a
-    href='https://www.linkedin.com/in/raphael-tunji-34a1073a/'
-    target='_blank'
-    rel='noreferrer'
+    href="https://www.linkedin.com/in/raphael-tunji-34a1073a/"
+    target="_blank"
+    rel="noreferrer"
   >
-    <i class="fa fa-linkedin-square" aria-hidden="true"></i> LinkedIn
+    <i class="fa fa-linkedin-square" aria-hidden="true"></i>{" "}
+    <span class="hideContact">LinkedIn</span>
   </a>,
   <a
-    href='https://www.mql5.com/en/users/archraphael'
-    target='_blank'
-    rel='noreferrer'
+    href="https://www.mql5.com/en/users/archraphael"
+    target="_blank"
+    rel="noreferrer"
   >
-    <img src="./mql5.png" alt="error" style={{width: '50px', height:'20px'}}></img> MQL5
+    <img
+      src="./mql5.png"
+      alt="error"
+      style={{ width: "50px", height: "20px" }}
+    ></img>{" "}
+    <span class="hideContact">MQL5</span>
   </a>,
-]
+];
 
 const ContactUs = () => {
   return (
     <div>
-      <CenteredHeadingUnderline title={'Contact Us'} />
-      <div className='contactUs'>
-        <div className='ctList'>
+      <CenteredHeadingUnderline title={"Contact Us"} />
+      <div className="contactUs">
+        <div className="ctList">
           <Lists listArray={contactHandles} />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ContactUs
+export default ContactUs;
