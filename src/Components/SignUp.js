6211abe77a3dbfaee26524f44a9e6c26052209e8
unknown <archraphr@gmail.com>
2022-11-21 20:58:11 +0100

    pushingToFirebase

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -7,19 +7,45 @@ const SignUp = () => {
     lastName: "",
     email: "",
     password: "",
-    confirmPassword:"",
+    confirmPassword: "",
   });
   const handleChange = (e) => {
     const name = e.target.name;
     const value = e.target.value;
     setPerson({ ...person, [name]: value });
   };
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
     if (person.firstName && person.email && person.password) {
       const newPerson = { ...person, id: new Date().getTime().toString() };
+      delete newPerson.confirmPassword;
       console.log(newPerson);
-      setPerson({ firstName: "", lastName:"", email: "", password: "",confirmPassword:"" });
+      setPerson({
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+      });
+      const { firstName, lastName, email, password } = newPerson;
+      const response = await fetch(
+        "https://archintel-api-default-rtdb.firebaseio.com/signupDB.json",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            firstName,
+            lastName,
+            email,
+            password,
+          }),
+        }
+      );
+      if (response.status === "200") {
+        console.log("Success signup");
+      }
     }
   };
   return (
